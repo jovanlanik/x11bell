985325f9d16979557fed4f21f5da1e49908cf9ea
Jovan Lanik <jox969@gmail.com>
2021-12-14 15:00:36 +0100

    cleanup

--- source.c
+++ source.c
@@ -1,34 +1,30 @@
 // X11 Bell
 // Copyright (c) 2021 Jovan Lanik
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
 
 int main(int argc, char *argv[]) {
-	if(argc < 2) exit(EXIT_FAILURE);
+	if(argc < 2) return 1;
 
 	Display *display = XOpenDisplay(NULL);
-	if(!display) exit(EXIT_FAILURE);
+	if(!display) return 1;
 
 	int major = XkbMajorVersion;
 	int minor = XkbMinorVersion;
-
-	if(!XkbLibraryVersion(&major, &minor)) exit(EXIT_FAILURE);
+	if(!XkbLibraryVersion(&major, &minor)) return 1;
 
 	major = XkbMajorVersion;
 	minor = XkbMinorVersion;
 	int xkb_event_base;
-
 	if(!XkbQueryExtension(display, NULL, &xkb_event_base, NULL, &major, &minor))
-		exit(EXIT_FAILURE);
+		return 1;
 
 	XkbSelectEvents(display, XkbUseCoreKbd, XkbBellNotifyMask, XkbBellNotifyMask);
 
 	unsigned int auto_ctrls, auto_values;
 	auto_ctrls = auto_values = XkbAudibleBellMask;
-
 	XkbSetAutoResetControls(display, XkbAudibleBellMask, &auto_ctrls, &auto_values);
 	XkbChangeEnabledControls(display, XkbUseCoreKbd, XkbAudibleBellMask, 0);
 
@@ -37,11 +33,9 @@ int main(int argc, char *argv[]) {
 		XNextEvent(display, &event);
 		if(((XkbEvent *)&event)->any.xkb_type != XkbBellNotify) continue;
 		pid_t pid = fork();
-		if(pid == -1) exit(EXIT_FAILURE);
-		else if(pid == 0) {
-			execvp(argv[1], &argv[1]);
-			exit(EXIT_FAILURE);
-		}
+		if(pid == 0) execvp(argv[1], &argv[1]);
+		else if(pid != -1) continue;
+		return 1;
 	}
 }
 
